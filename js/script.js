6ec94b2be94af0f88b88e0e9a59e896e686d290a
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-17 06:05:33 -0300

    Adicionando options do select
    As options são definidas a partir do objeto obtido da chamada de API

--- js/script.js
+++ js/script.js
@@ -5,11 +5,27 @@ const currency2 = document.getElementById('currency2')
 
 document.addEventListener('DOMContentLoaded',callAPI)
 
+function insertOptionsInSelect(currencies,selectHTML) {
+    currencies.forEach((element,index) => {
+        const createOption = document.createElement('option')
+        
+        selectHTML.appendChild(createOption)
+
+        selectHTML.children[index].innerHTML = element
+        selectHTML.children[index].value = `${element}`
+    }) 
+}
+
 async function callAPI() {
     const res = await fetch('https://cdn.moeda.info/api/latest.json')
 
     const data = await res.json()
 
+    const currencies = Object.keys(data.rates)
+
+    insertOptionsInSelect(currencies,currency1)
+    insertOptionsInSelect(currencies,currency2)
+    
     
 }
 
