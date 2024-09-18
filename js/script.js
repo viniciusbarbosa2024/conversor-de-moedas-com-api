4d67a025f73f27694453dcfc8f14b33bb9e13ec0
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-09-17 21:13:37 -0300

    Iniciando função de conversão

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const value2 = document.getElementById('value2')
 const currency1 = document.getElementById('currency1')
 const currency2 = document.getElementById('currency2')
 
-document.addEventListener('DOMContentLoaded',callAPI)
+document.addEventListener('DOMContentLoaded',useApiData)
+
+currency1.addEventListener('change',convertCurrency)
+currency2.addEventListener('change',convertCurrency)
 
 function insertOptionsInSelect(currencies,selectHTML) {
     currencies.forEach((element,index) => {
@@ -24,10 +27,25 @@ function clearCurrenciesList(currenciesList) {
     return currenciesList
 }
 
-async function callAPI() {
+async function convertCurrency() {
+   const data = await getDataFromApi()
+   
+   const currency1ValueInDollars = data.rates[`${currency1.value}`]
+   const currency2ValueInDollars = data.rates[`${currency2.value}`]
+
+   
+}
+
+async function getDataFromApi() {
     const res = await fetch('https://cdn.moeda.info/api/latest.json')
 
     const data = await res.json()
+
+    return data
+}
+
+async function useApiData() {
+    const data = await getDataFromApi()
 
     const currencies = clearCurrenciesList(Object.keys(data.rates))
 
@@ -35,8 +53,14 @@ async function callAPI() {
 
     insertOptionsInSelect(currencies,currency1)
     insertOptionsInSelect(currencies,currency2)
-    
+
+    currency1.value = 'USD'
+    currency2.value = 'USD'
+
+    console.log(data)
     
 }
+
+
 
 
